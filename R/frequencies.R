8f41a13c27ab47a2d167e5ce1c7eeceff24bba5d
Scotty Riding <scott@y2analytics.com>
2019-01-02 15:49:19 -0700

    Removed freq_ms since it's poorly executed and rarely used.

--- R/frequencies.R
+++ R/frequencies.R
@@ -35,36 +35,6 @@ freqs <- function(dataset, ..., nas = TRUE, wt = NULL, prompt = F, digits = 2) {
 # Create a redundant function for convenience/backwards compatibility.
 freq <- freqs
 
-#' Run frequencies for multiple select survey questions.
-#'
-#' @param dataset A dataframe.
-#' @param ... The unquoted names of a set of variables in the dataframe that
-#' represent a single multiple select question on a survey. Each variable
-#' should be encoded as non-NA for a selection, NA for no selection.
-#' @param wt The unquoted name of a weighting variable in the dataframe.
-#' @param var_name An optional variable name to assign to the results. If
-#' omitted, it will derive a name by slicing off _[digit] suffixes.
-#' @param prompt Boolean, whether or not to include the prompt in the dataframe.
-#' @param digits Integer, number of significant digits for rounding. Default is 2,
-#' which results in an integer percentage.
-#' @return A dataframe with the variable names, prompts, values, labels, counts,
-#' and percents.
-#' @export
-freq_ms <- function(dataset, ..., wt = NULL, var_name = NULL, prompt = F, digits = 2) {
-  weight <- dplyr::enquo(wt)
-  freqs(dataset, ..., wt = !!weight, prompt = prompt, digits = digits) %>%
-    filter(
-      !is.na(value)
-    ) %>%
-    mutate(
-      variable = ifelse(
-        is.null(var_name),
-        derive_var_name(variable),
-        var_name
-      )
-    )
-}
-
 ##### Private functions #####
 
 freq_var <- function(dataset, variable, nas = TRUE, wt = NULL, prompt = F, digits = 2) {
@@ -160,8 +130,3 @@ base_ns <- function(dataset, variable, weight) {
     )
 }
 
-derive_var_name <- function(variable_names) {
-  # Just chops off the final _1 or _x2. We can make this more sophisticated
-  # later if this isn't working right for us.
-  stringr::str_replace(variable_names, '_[x\\d]+$', '')
-}
