94406ed219d015cef707763003e1593d152a8fad
Scotty Riding <scott@y2analytics.com>
2019-01-02 14:51:04 -0700

    Removing testing harness for removed functionality.

--- tests/testthat/testcrosstabs.R
+++
@@ -1,1 +0,0 @@
-context("Tests on crosstabs functions")
--- tests/testthat/testhelloworld.R
+++
@@ -1,7 +0,0 @@
-context("y2clerk's first testing suite")
-
-test_that("Hello World",{
-  hello <- T
-  world <- T
-  expect_equal(hello, world)
-})
