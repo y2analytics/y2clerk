fc29b0f02890f0fc83eacc3f6020de5594517f3c
Jacob Eliason <jacobeliason@gmail.com>
2019-06-19 12:17:26 -0400

    Trivial updates.

--- tests/testthat/test-freqs-means.R
+++ tests/testthat/test-freqs-means.R
@@ -8,29 +8,29 @@ library(y2clerk)
 library(tidyverse)
 library(labelled)
 
-set.seed(123)
+set.seed(1)
 
 # test data ---------------------------------------------------------------
 
-responses <-
+responses <- {
   data.frame(
 
-    # continuous numeric, no label, no NA
+    # continuous numeric, no variable label, no NA
     q0 = sample(x = datasets::swiss$Agriculture, size = 25, replace = T),
 
-    # continuous numeric, labelled, incl. NA
-    q1 = sample(x = c(datasets::swiss$Agriculture, NA), size = 25, prob = c(rep(.95/47,47), 0.05), replace = T),
+    # continuous numeric, variable label, incl. NA
+    q1 = sample(x = c(datasets::swiss$Agriculture, NA), size = 25, prob = c(rep(.9/47,47), 0.1), replace = T),
 
-    # factor (numbers), no label
+    # factor (numbers), no value labels
     q2 = sample(x = datasets::Orange$Tree, size = 25, replace = T),
 
-    # character, no label
+    # character, no value labels
     q3 = sample(stringr::fruit, 25, prob = 1/(1:80 * sum(1/(1:80))), replace = T),
 
-    # numeric values, discrete labels
+    # numeric values, discrete value labels
     q4 = sample(1:8, 25, replace = T),
 
-    # character values, discrete labels
+    # character values, discrete value labels
     q5 = sample(letters[1:4], 25, prob = c(0.4,0.3,0.2,0.1), replace = T),
 
     # numeric weights
@@ -64,7 +64,7 @@ responses <-
     w = "Weights"
   ) %>%
 
-  as_tibble()
+  as_tibble()}
 
 # tests -------------------------------------------------------------------
 
@@ -80,7 +80,7 @@ test_that("test data is correct", {
 
 context("numeric variables")
 
-test_that("NAs not present, nas = T: result is correct", {
+test_that("NAs not present, nas = T: n & result are correct", {
   expect_equivalent(responses %>%
                       select(q0) %>%
                       freqs(stat = "mean") %>%
@@ -88,6 +88,15 @@ test_that("NAs not present, nas = T: result is correct", {
                       pull(),
 
                     round(mean(responses$q0),2)
+  )
+
+  expect_equivalent(responses %>%
+                      select(q0) %>%
+                      freqs(stat = "mean", nas = T) %>%
+                      select(n) %>%
+                      pull(),
+
+                    nrow(responses[!is.na(responses$q0),])
   )
 })
 
@@ -113,9 +122,7 @@ test_that("NAs not present, nas = F: n & result are correct", {
 test_that("NAs present, nas = T: throws error", {
   expect_error(responses %>%
                  select(q1) %>%
-                 freqs(stat = "mean") %>%
-                 select(result) %>%
-                 pull()
+                 freqs(stat = "mean")
   )
 })
 
@@ -174,10 +181,89 @@ test_that("column with value labels input: throws error", {
   )
 })
 
+test_that("column with value labels input: (potentially misleading) result is correct
+          after labels are removed", {
+  expect_equivalent(
+    responses %>%
+      mutate(q4 = as.numeric(q4)) %>%
+      select(q4) %>%
+      freqs(stat = 'mean') %>%
+      select(result) %>%
+      pull(),
+    mean(responses$q4)
+  )
+})
 
+test_that("column with value labels input: answer is correct after labels removed (even if potentially misleading)", {
+  expect_equivalent(
+    responses %>%
+      select(q4) %>%
+      remove_labels() %>%
+      freqs(stat = 'mean') %>%
+      select(result) %>%
+      pull(),
+    mean(responses$q4)
+  )
+})
 
+#
 
+context("weights")
 
+test_that("using weights: equivalent to weighted.mean() output", {
+  expect_equal(
+    responses %>%
+      select(q1, w) %>%
+      freqs(stat = 'mean', nas = F, wt = w) %>%
+      select(result) %>%
+      pull(),
+
+    stats::weighted.mean(x = responses$q1,
+                         w = responses$w,
+                         na.rm = T) %>%
+      round(2)
+  )
+})
+
+#
+
+context("prompt")
+
+test_that("using prompt: variable label is correctly output", {
+  expect_equal(
+    responses %>%
+      select(q1) %>%
+      freqs(stat = 'mean', nas = F, prompt = T) %>%
+      select(prompt) %>%
+      pull(),
+
+    responses %>%
+      select(q1) %>%
+      var_label() %>%
+      deframe()
+
+  )
+})
+
+#
+
+context("digits")
+
+test_that("using digits: output is precise to multiple decimal places", {
+  expect_equal(
+    responses %>%
+      select(w) %>%
+      freqs(stat = 'mean', digits = 6, nas = F) %>%
+      select(result) %>%
+      pull(),
+
+    responses %>%
+      select(w) %>%
+      pull() %>%
+      mean(na.rm = T) %>%
+      round(digits = 6)
+  )
+})
 
 
 
