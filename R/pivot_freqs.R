4f8e49304de171df97b71731c2c7f2114eda5d21
Luke Carter <lukemcarter12@gmail.com>
2023-01-20 15:43:38 -0500

    Added error message if in case someone passes an object that is not formatted like a freqs() output. Changed data for tests and examples. Added a few tests.

--- R/pivot_freqs.R
+++ R/pivot_freqs.R
@@ -11,16 +11,16 @@
 #' @return A wide tibble of frequencies with one row for each group (by default)
 #' @export
 #' @examples
-#'   frequencies <- ToothGrowth %>%
-#'     dplyr::group_by(supp) %>%
-#'       y2clerk::freqs(dose) %>%
+#'   frequencies <- forcats::gss_cat %>%
+#'     dplyr::group_by(year) %>%
+#'       y2clerk::freqs(marital) %>%
 #'       pivot_freqs()
 
 pivot_freqs <-
   function(
     dataset,
     columns_var = label
-    ) {
+  ) {
 
     label <- NULL
     pivot_errors(dataset)
@@ -30,12 +30,12 @@ pivot_freqs <-
         .data$group_var,
         .data$label,
         .data$result
-        ) %>%
+      ) %>%
       tidyr::pivot_wider(
         names_from = {{ columns_var }},
         values_from = .data$result,
         values_fill = 0
-        )
+      )
   }
 
 
@@ -44,16 +44,22 @@ pivot_freqs <-
 
 pivot_errors <- function(dataset) {
 
+  if (
+    !'label' %in% names(dataset) |
+    !'result' %in% names(dataset)
+  ) {
+    stop('Input data must contain a "label" column and a "result" column. Ensure you are passing the output from a freqs() call.')
+  }
+
   if (unique(dataset$label)[1] == '' &
       length(unique(dataset$label)) == 1) {
-    stop('Your frequencies label column is blank. Please provide labels on which to pivot')
+    stop('Your frequencies label column is blank. Please provide labels on which to pivot.')
   }
 
   if (
-    sum(names(dataset) %in% 'group_var') != 1
+    sum(names(dataset) == 'group_var') != 1
   ) {
-    stop('Your frequencies does not contain a group_var. It must have a group_var to pivot correctly')
+    stop('Your frequencies does not contain a group_var. It must have a group_var to pivot correctly.')
   }
 
 }
-
--- tests/testthat/test-pivot_freqs.R
+++ tests/testthat/test-pivot_freqs.R
@@ -8,9 +8,8 @@ library(forcats)
 library(stringr)
 
 ### Data
-ToothGrowth
-names(ToothGrowth)
-
+gss_cat
+names(gss_cat)
 
 # Tests -------------------------------------------------------------------
 ### Context
@@ -19,71 +18,113 @@ context("Tests on pivot_freqs")
 
 ### Column names
 test_that("Column names", {
-  frequencies_base <- ToothGrowth %>%
-    group_by(supp) %>%
-    freqs(dose)
+  frequencies_base <- gss_cat %>%
+    group_by(year) %>%
+    freqs(marital)
   frequencies_base
 
   frequencies_pivoted <- frequencies_base %>% pivot_freqs()
   frequencies_pivoted
 
-  names_actual <- frequencies_pivoted  %>% names()
+  names_actual <- frequencies_pivoted %>% names()
   names_expected <- c(
     'group_var',
-    '0.5',
-    '1',
-    '2'
-    )
+    'No answer',
+    'Never married',
+    'Separated',
+    'Divorced',
+    'Widowed',
+    'Married'
+  )
   expect_equal(names_actual, names_expected)
 })
 
 
+test_that("Column names with two group vars", {
+  frequencies_pivoted <- gss_cat %>%
+    group_by(year, marital) %>%
+    freqs(race) %>%
+    pivot_freqs()
+  frequencies_pivoted
+  nrows <- nrow(frequencies_pivoted)
+  names_cols <- names(frequencies_pivoted)
+
+  names_actual <- frequencies_pivoted %>% names()
+  names_expected <- c(
+    'group_var2',
+    'group_var',
+    'Other',
+    'Black',
+    'White',
+    'Not applicable'
+  )
+  expect_equal(names_actual, names_expected)
+})
+
+### Group_var levels
 test_that("Each row is a group_var level", {
-  frequencies_pivoted <- ToothGrowth %>%
-    group_by(supp) %>%
-    freqs(dose) %>%
+  frequencies_pivoted <- gss_cat %>%
+    group_by(year) %>%
+    freqs(marital) %>%
     pivot_freqs()
   frequencies_pivoted
   nrows <- length(frequencies_pivoted$group_var)
-  names_rows <- levels(frequencies_pivoted$group_var)
+  names_rows <- as.character(frequencies_pivoted$group_var)
 
-  expect_equal(nrows, 2)
-  expect_equal(names_rows, c('OJ', 'VC'))
+  expect_equal(nrows, 8)
+  expect_equal(names_rows, c('2000',
+                             '2002',
+                             '2004',
+                             '2006',
+                             '2008',
+                             '2010',
+                             '2012',
+                             '2014'))
 })
 
 
 ### columns_var - pivot other way
 test_that("Pivot on group_var", {
-  frequencies_pivoted <- ToothGrowth %>%
-    group_by(supp) %>%
-    freqs(dose) %>%
+  frequencies_pivoted <- gss_cat %>%
+    group_by(year) %>%
+    freqs(marital) %>%
     pivot_freqs(group_var)
   frequencies_pivoted
   nrows <- nrow(frequencies_pivoted)
   names_cols <- names(frequencies_pivoted)
 
-  expect_equal(nrows, 3)
-  expect_equal(names_cols, c('label', 'OJ', 'VC'))
+  expect_equal(nrows, 6)
+  expect_equal(names_cols, c('label', '2000', '2002', '2004', '2006',
+                             '2008', '2010', '2012', '2014'))
 })
 
 
 ### Errors
-test_that("pivot_freqs error testing", {
+test_that("pivot_freqs error testing - blank label column", {
   expect_error(
-    frequencies <- ToothGrowth %>%
-      freqs(len, stat = 'mean') %>%
+    frequencies <- gss_cat %>%
+      freqs(age, stat = 'mean', nas = FALSE) %>%
       pivot_freqs(),
-    'Your frequencies label column is blank. Please provide labels on which to pivot',
+    'Your frequencies label column is blank. Please provide labels on which to pivot.',
     fixed = TRUE
   )
 })
 
-test_that("pivot_freqs error testing", {
+test_that("pivot_freqs error testing - missing group_var", {
   expect_error(
-    frequencies <- ToothGrowth %>%
-      freqs(supp) %>%
+    frequencies <- gss_cat %>%
+      freqs(marital) %>%
       pivot_freqs(),
-    'Your frequencies does not contain a group_var. It must have a group_var to pivot correctly',
+    'Your frequencies does not contain a group_var. It must have a group_var to pivot correctly.',
+    fixed = TRUE
+  )
+})
+
+test_that("pivot_freqs error testing - missing label or result column", {
+  expect_error(
+    frequencies <- gss_cat %>%
+      pivot_freqs(),
+    'Input data must contain a "label" column and a "result" column. Ensure you are passing the output from a freqs() call.',
     fixed = TRUE
   )
 })
