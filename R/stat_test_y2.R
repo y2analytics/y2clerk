a77f13c73a2a70068e203b81ed6b6712c81dfcc8
Canyen Heimuli <46830067+canyenheimuli@users.noreply.github.com>
2024-02-26 12:38:39 -0500

    Update stat_test_y2.R
    Added handling for grouped multi-select question freqs

--- R/stat_test_y2.R
+++ R/stat_test_y2.R
@@ -86,7 +86,7 @@ sig_test_y2 <- function(
     wt = NULL,
     layout = c('tall', 'wide')
 ) {
-
+  
   ## Error for missing dataset argument
   if (missing(dataset)) {
     
@@ -104,7 +104,7 @@ sig_test_y2 <- function(
     )
     
   }
-    
+  
   ## Error for labelled double group_var
   if (haven::is.labelled(frequencies$group_var) == TRUE) {
     
@@ -132,6 +132,9 @@ sig_test_y2 <- function(
     dplyr::distinct(.data$variable) %>%
     dplyr::pull()
   
+  # Running list of currently filtered stems
+  filtered_stems <- vector()
+  
   # Define group_var levels from frequencies object
   group_levels <- frequencies %>%
     dplyr::count(.data$group_var) %>%
@@ -148,7 +151,7 @@ sig_test_y2 <- function(
   tested_freqs <- frequencies %>%
     dplyr::mutate(sig = '')
   
-  for (var_name in var_names){
+  for (var_name in var_names) {
     
     # Define value levels from frequencies object
     if (haven::is.labelled(dataset[[var_name]])) {
@@ -176,7 +179,6 @@ sig_test_y2 <- function(
     for (i in value_levels) {
       
       # Process updates
-      
       if (haven::is.labelled(dataset[[var_name]])) {
         
         haven_val_label <- frequencies %>%
@@ -212,6 +214,50 @@ sig_test_y2 <- function(
         
       }
       
+      # Filter dataset to non all NULL responses based on stem
+      var_stem <- stringr::str_remove(var_name, '_[0-9]+$')
+
+      if (!var_stem %in% filtered_stems) {
+        
+        # Filter all NA rows for each stem
+        filtered_dataset <- dataset %>%
+          dplyr::mutate(
+            ns = rowSums(
+              dplyr::across(
+                .cols = dplyr::matches(stringr::str_c(var_stem, '_[0-9]+$')),
+                .fns = ~ifelse(
+                  is.na(.x),
+                  FALSE,
+                  TRUE
+                )
+              )
+            ),
+            # Set remaining NAs to zero as not to confuse test data
+            dplyr::across(
+              .cols = dplyr::matches(stringr::str_c(var_stem, '_[0-9]+$')),
+              .fns = ~ifelse(
+                is.na(.x),
+                0,
+                .x
+              )
+            )
+          ) %>% 
+          dplyr::filter(
+            ns > 0
+          ) %>%
+          dplyr::select(
+            -ns
+          )
+        
+        # Add stem to list so it's not filtered again
+        filtered_stems <- append(filtered_stems, var_stem)
+        
+      } else if (!exists("filtered_dataset")) {
+        
+        filtered_dataset <- dataset
+        
+      }
+    
       # All group_level pairwise combinations
       for (j in group_levels) {
         for (k in group_levels) {
@@ -220,12 +266,10 @@ sig_test_y2 <- function(
           if (j != k) {
             
             # Proportions check
-            
             if (haven::is.labelled(dataset[[var_name]])) {
               
               # For haven labelled vars, reference value
-              
-              Px <- frequencies %>%
+              px <- frequencies %>%
                 dplyr::filter(
                   .data$variable == var_name,
                   .data$group_var == j,
@@ -233,7 +277,7 @@ sig_test_y2 <- function(
                 ) %>%
                 dplyr::pull(.data$result)
               
-              Py <- frequencies %>%
+              py <- frequencies %>%
                 dplyr::filter(
                   .data$variable == var_name,
                   .data$group_var == k,
@@ -244,8 +288,7 @@ sig_test_y2 <- function(
             } else {
               
               # Reference label for all else
-              
-              Px <- frequencies %>%
+              px <- frequencies %>%
                 dplyr::filter(
                   .data$variable == var_name,
                   .data$group_var == j,
@@ -253,7 +296,7 @@ sig_test_y2 <- function(
                 ) %>%
                 dplyr::pull(.data$result)
               
-              Py <- frequencies %>%
+              py <- frequencies %>%
                 dplyr::filter(
                   .data$variable == var_name,
                   .data$group_var == k,
@@ -264,30 +307,26 @@ sig_test_y2 <- function(
             }
             
             # In cases where no one in group answered
-            
-            if (rlang::is_empty(Px)) {
+            if (rlang::is_empty(px)) {
               
-              Px <- 0
-              
-            }
-            
-            if (rlang::is_empty(Py)) {
-              
-              Py <- 0
+              px <- 0
               
             }
             
-            # Only where Px is greater than Py
+            if (rlang::is_empty(py)) {
+              
+              py <- 0
+              
+            }
             
-            if (Px > Py) {
+            # Only where px is greater than py
+            if (px > py) {
               
               # Unweighted
-              
               if (weight_exists == FALSE) {
                 
                 # Set up testing data
-                
-                test_data <- dataset %>%
+                test_data <- filtered_dataset %>%
                   dplyr::select(
                     test_var = tidyselect::all_of(var_name),
                     group = {{ banner_var }}
@@ -307,7 +346,6 @@ sig_test_y2 <- function(
                   )
                 
                 # Create survey design object
-                
                 surv_object <- survey::svydesign(
                   id =~ 1,
                   weights = NULL,
@@ -317,12 +355,10 @@ sig_test_y2 <- function(
               }
               
               # Weighted
-              
               if (weight_exists == TRUE) {
                 
                 # Set up testing data
-                
-                test_data <- dataset %>%
+                test_data <- filtered_dataset %>%
                   dplyr::select(
                     test_var = tidyselect::all_of(var_name),
                     group = {{ banner_var }},
@@ -343,7 +379,6 @@ sig_test_y2 <- function(
                   )
                 
                 # Create survey design object
-                
                 surv_object <- survey::svydesign(
                   id =~ 1,
                   weights =~ weight,
@@ -353,7 +388,6 @@ sig_test_y2 <- function(
               }
               
               # Get test results
-              
               p_value <- survey::svychisq(
                 ~test_var + group,
                 surv_object
@@ -361,13 +395,11 @@ sig_test_y2 <- function(
                 purrr::pluck('p.value')
               
               # Get legend code
-              
               group_letter <- sig_codes %>%
                 dplyr::filter(group_levels == k) %>%
                 dplyr::pull(.data$reference)
               
               # FDR correction (default; used in Q crosstabs)
-              
               p_value <- stats::p.adjust(
                 p_value,
                 method = 'fdr',
@@ -381,16 +413,14 @@ sig_test_y2 <- function(
                 TRUE ~ ''
               )
               
-              # Skip for any Px < Py
-              
+            # Skip for any px < py
             } else {
               
               code_result = ''
               
             }
             
-            # Skip if j == k
-            
+          # Skip if j == k
           } else {
             
             code_result = ''
@@ -443,7 +473,6 @@ sig_test_y2 <- function(
   }
   
   ## Final appending group level references
-  
   sig_codes_refs <- sig_codes %>%
     dplyr::mutate(
       group_levels = unlist(
@@ -482,8 +511,8 @@ sig_test_y2 <- function(
     dplyr::select(-'reference')
   
   ## Layout options
-  # Normal Freqs Layout (tall)
   
+  # Normal Freqs Layout (tall)
   if (layout == 'tall') {
     
     return(tested_freqs)
@@ -491,7 +520,6 @@ sig_test_y2 <- function(
   }
   
   # Crosstab Layout (wide)
-  
   if (layout == 'wide') {
     
     # Get wide percentages
@@ -548,7 +576,6 @@ sig_test_y2 <- function(
       )
     
     # Combine
-    
     xtab <- data.frame()
     
     for (i in 1:nrow(xtab_results)) {
