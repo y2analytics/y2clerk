bc0cd7c46b62aa83cbf3ca0a55871b08276029d6
Adam Hubbs <amhubbs17@gmail.com>
2026-07-02 14:13:38 -0600

    Fix documentation for p.adjust.methods

---
+++ R/y2clerk-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats p.adjust.methods
+## usethis namespace: end
+
+## mockable bindings: start
+## mockable bindings: end
+NULL
